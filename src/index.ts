fd2009da00fa8f6f30cda1e49c0b29599b3cbe75
poplark <songkang666@126.com>
2020-05-21 22:15:45 +0800

    [feat] - format audio, video reports class

--- src/index.ts
+++ src/index.ts
@@ -1,114 +1,230 @@
 // audio
 class AudioInputReports {
   /**
-   * chrome - inbound-rtp
-   * firefox - inbound-rtp
-   * safari - inbound-rtp
+   * chrome - { type: inbound-rtp, mediaType: audio }
+   * firefox - { type: inbound-rtp, mediaType: audio }
+   * safari - { type: inbound-rtp, mediaType: audio }
    */
   bytesReceived: number;
 
   /**
-   * chrome - inbound-rtp
-   * firefox - inbound-rtp
-   * safari - inbound-rtp
+   * chrome - { type: inbound-rtp, mediaType: audio }
+   * firefox - { type: inbound-rtp, mediaType: audio }
+   * safari - { type: inbound-rtp, mediaType: audio }
    */
   packetsReceived: number;
 
   /**
-   * chrome - inbound-rtp
-   * firefox - inbound-rtp
-   * safari - inbound-rtp
+   * chrome - { type: inbound-rtp, mediaType: audio }
+   * firefox - { type: inbound-rtp, mediaType: audio }
+   * safari - { type: inbound-rtp, mediaType: audio }
    */
   jitter: number;
 
   /**
-   * chrome - inbound-rtp
-   * firefox - inbound-rtp
-   * safari - inbound-rtp
+   * chrome - { type: inbound-rtp, mediaType: audio }
+   * firefox - { type: inbound-rtp, mediaType: audio }
+   * safari - { type: inbound-rtp, mediaType: audio }
    */
   packetsLost: number;
 
   /**
-   * chrome - track
-   * safari - track
+   * chrome - { type: track, kind: audio }
+   * safari - { type: track }
    */
-  audioLevel?: number;
+  audioLevel: number;
 
   /**
-   * chrome - track
+   * chrome - { type: track, kind: audio }
    */
   totalAudioEnergy?: number;
-
-  /**
-   * firefox - remote-outbound-rtp
-   * safari - outbound-rtp
-   */
-  bytesSent?: number;
-
-  /**
-   * firefox - remote-outbound-rtp
-   * safari - outbound-rtp
-   */
-  packetsSent?: number;
 }
 
 class AudioOutputReports {
   /**
-   * chrome - outbound-rtp
-   * firefox - outbound-rtp
-   * safari - outbound-rtp
+   * chrome - { type: outbound-rtp, mediaType: audio }
+   * firefox - { type: outbound-rtp, mediaType: audio }
+   * safari - { type: outbound-rtp, mediaType: audio }
    */
   bytesSent: number;
 
   /**
-   * chrome - outbound-rtp
-   * firefox - outbound-rtp
-   * safari - outbound-rtp
+   * chrome - { type: outbound-rtp, mediaType: audio }
+   * firefox - { type: outbound-rtp, mediaType: audio }
+   * safari - { type: outbound-rtp, mediaType: audio }
    */
   packetsSent: number;
 
   /**
-   * chrome - remote-inbound-rtp
-   * firefox - remote-inbound-rtp
+   * chrome - { type: remote-inbound-rtp, kind: audio }
+   * firefox - { type: remote-inbound-rtp, kind: audio }
    */
-  jitter?: number;
+  jitter: number;
   /**
-   * chrome - remote-inbound-rtp
-   * firefox - remote-inbound-rtp
+   * chrome - { type: remote-inbound-rtp, kind: audio }
+   * firefox - { type: remote-inbound-rtp, kind: audio }
    */
-  packetsLost?: number;
+  packetsLost: number;
   /**
-   * chrome - remote-inbound-rtp
-   * firefox - remote-inbound-rtp
+   * chrome - { type: remote-inbound-rtp, kind: audio }
+   * firefox - { type: remote-inbound-rtp, kind: audio }
    */
-  roundTripTime?: number;
+  roundTripTime: number;
 
   /**
-   * chrome - media-source 
+   * chrome - { type: media-source, kind: audio }
    */
   audioLevel?: number;
 
   /**
-   * chrome - media-source 
+   * chrome - { type: media-source, kind: audio }
    */
   totalAudioEnergy?: number;
-
-  /**
-   * firefox - remote-inbound-rtp
-   */
-  bytesReceived?: number;
-
-  /**
-   * firefox - remote-inbound-rtp
-   */
-  packetsReceived?: number;
 }
 
 
 // video
-
 class VideoInputReports {
+  /**
+   * chrome - { type: track, kind: video }
+   * safari - { type: track }
+   */
+  frameWidth: number;
+
+  /**
+   * chrome - { type: track, kind: video }
+   * safari - { type: track }
+   */
+  frameHeight: number;
+
+  /**
+   * chrome - { type: inbound-rtp, mediaType: video }
+   * firefox - { type: inbound-rtp, mediaType: video }
+   * safari - { type: inbound-rtp, mediaType: video }
+   */
+  bytesReceived: number;
+
+  /**
+   * chrome - { type: inbound-rtp, mediaType: video }
+   * firefox - { type: inbound-rtp, mediaType: video }
+   * safari - { type: inbound-rtp, mediaType: video }
+   */
+  packetsReceived: number;
+
+  /**
+   * chrome - { type: track, kind: video }
+   * safari - { type: track }
+   */
+  framesReceived: number;
+
+  /**
+   * chrome - { type: track, kind: video }
+   * firefox - { type: inbound-rtp, kind: video }
+   * safari - { type: track }
+   */
+  framesDecoded: number;
+
+  /**
+   * chrome - { type: track, kind: video }
+   * safari - { type: track }
+   */
+  framesDropped: number;
+
+  /**
+   * firefox - { type: inbound-rtp, kind: video }
+   */
+  jitter: number;
+
+  /**
+   * chrome - { type: inbound-rtp, kind: video }
+   * firefox - { type: inbound-rtp, kind: video }
+   * safari - { type: inbound-rtp, mediaType: video }
+   */
+  packetsLost: number;
 }
 
 class VideoOutputReports {
+  /**
+   * chrome - { type: track, kind: video }
+   * safari - { type: track }
+   */
+  frameWidth: number;
+
+  /**
+   * chrome - { type: track, kind: video }
+   * safari - { type: track }
+   */
+  frameHeight: number;
+
+  /**
+   * chrome - { type: outbound-rtp, mediaType: video }
+   * firefox = { type: outbound-rtp, mediaType: video}
+   * safari = { type: outbound-rtp, mediaType: video}
+   */
+  bytesSent: number;
+
+  /**
+   * chrome - { type: outbound-rtp, mediaType: video }
+   * firefox = { type: outbound-rtp, mediaType: video}
+   * safari = { type: outbound-rtp, mediaType: video}
+   */
+  packetsSent: number;
+
+  /**
+   * chrome - { type: remote-inbound-rtp, kind: video }
+   * firefox - { type: remote-inbound-rtp, kind: video }
+   */
+  packetsLost: number;
+
+  /**
+   * chrome - { type: remote-inbound-rtp, kind: video }
+   * firefox - { type: remote-inbound-rtp, kind: video }
+   */
+  jitter: number
+
+  /**
+   * chrome - { type: remote-inbound-rtp, kind: video }
+   * firefox - { type: remote-inbound-rtp, kind: video }
+   */
+  roundTripTime: number
+
+  /**
+   * chrome - { type: track, kind: video }
+   * safari - { type: track }
+   */
+  framesSent: number;
+
+  /**
+   * chrome - { type: outbound-rtp, mediaType: video }
+   * firefox = { type: outbound-rtp, mediaType: video}
+   * safari = { type: outbound-rtp, mediaType: video}
+   */
+  framesEncoded: number
+}
+
+// paire
+class CandidatePairReports {
+  /**
+   * chrome - candidate-pair
+   */
+  bytesSent: number;
+
+  /**
+   * chrome - candidate-pair
+   */
+  bytesReceived: number;
+
+  /**
+   * chrome - candidate-pair (currentRoundTripTime)
+   */
+  currentRoundTripTime: number;
+}
+
+// ============ legacy =============
+class RTCLegacyStatsReport {
+  id: string;
+  type: string;
+}
+
+class AudioOutputLegacyReports {
 }
